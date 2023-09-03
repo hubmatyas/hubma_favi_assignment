6f68bf2d772d872c843f3beac3835e8142bd3b0b
Matyáš Hub <hubmatyas@gmail.com>
2023-09-03 13:02:05 +0200

    backend endpoint update

--- server/main.go
+++ server/main.go
@@ -9,12 +9,13 @@ import (
 )
 
 type ProductDetail struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Photo       string `json:"photo"`
-	Description string `json:"description"`
-	Price       string `json:"price"`
-	Parameters  []struct {
+	ID                string   `json:"id"`
+	Name              string   `json:"name"`
+	Photo             string   `json:"photo"`
+	Description       string   `json:"description"`
+	Price             string   `json:"price"`
+	SimilarProductIDs []string `json:"similarProductIds"`
+	Parameters        []struct {
 		Name  string `json:"name"`
 		Value string `json:"value"`
 	} `json:"parameters"`
@@ -22,21 +23,38 @@ type ProductDetail struct {
 
 var productCache []ProductDetail
 
-func getDetail(c *gin.Context) {
-	id := c.Query("id")
+func getProducts(c *gin.Context) {
+	c.JSON(200, productCache)
+}
 
-	if id == "" {
-		c.JSON(200, productCache)
-		return
-	}
-
+func getProduct(c *gin.Context) {
+	id := c.Param("id")
 	for _, product := range productCache {
 		if product.ID == id {
 			c.JSON(200, product)
 			return
 		}
 	}
+	c.JSON(404, gin.H{"error": "Product not found"})
+}
 
+func getSimilar(c *gin.Context) {
+	id := c.Param("id")
+	var similarProducts []ProductDetail
+
+	for _, product := range productCache {
+		if product.ID == id {
+			for _, similarID := range product.SimilarProductIDs {
+				for _, similarProduct := range productCache {
+					if similarProduct.ID == similarID {
+						similarProducts = append(similarProducts, similarProduct)
+					}
+				}
+			}
+			c.JSON(200, similarProducts) // Normálně bych filtroval data similarProducts, ale pro tento příklad je tam tak akorát to, co potřebujeme
+			return
+		}
+	}
 	c.JSON(404, gin.H{"error": "Product not found"})
 }
 
@@ -52,6 +70,8 @@ func main() {
 	}
 
 	r := gin.Default()
-	r.GET("/detail", getDetail)
+	r.GET("/products", getProducts)
+	r.GET("/products/:id", getProduct)
+	r.GET("/products/:id/similar", getSimilar)
 	r.Run(":8000")
 }
