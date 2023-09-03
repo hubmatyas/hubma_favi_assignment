b0b5a57d782e3d9032c92f838c0bdd6e6d693189
Matyáš Hub <hubmatyas@gmail.com>
2023-09-03 16:33:48 +0200

    cors update

--- server/main.go
+++ server/main.go
@@ -76,6 +76,15 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Next()
+	})
+
 	r.GET("/products", getProducts)
 	r.GET("/products/:id", getProduct)
 	r.GET("/products/:id/similar", getSimilar)
